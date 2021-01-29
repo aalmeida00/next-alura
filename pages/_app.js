c4e36fe9ba2ce4ff1397877a4d5ca62c124d6704
Andre Almeida <andre-silva78@hotmail.com>
2021-01-28 22:24:32 -0300

    refactor: css basic reset

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,28 @@ import { createGlobalStyle, ThemeProvider } from "styled-components";
 import db from "../db.json";
 
 const GlobalStyle = createGlobalStyle`
+  *{
+    box-sizing: border-box;
+  }
   body {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+
+  html, body{
+    min-height: 100vh;
+  }
+
+  #__next{
+    flex: 1;
+    display: flex;
+    flex-direction: column:
   }
 `;
 
@@ -14,8 +32,8 @@ const theme = db.theme;
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
